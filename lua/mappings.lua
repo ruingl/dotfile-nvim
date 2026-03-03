72366346fcbe886b867b060efa546b4059fb215f
ruingl <ruireogo@gmail.com>
2026-03-03 17:47:44 +0800

    auto nvim tree togfle

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- ruingl: pls close nvim tree
+map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
